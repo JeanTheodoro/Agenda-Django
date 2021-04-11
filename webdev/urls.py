5ed143a071b9538eb82b208734d86f21d2421ffe
JeanTheodoro <jean.theodorosilva@gmail.com>
2021-04-11 12:52:49 -0700

    Creates test and form to read task

--- webdev/urls.py
+++ webdev/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .home_view import home
+from django.urls import path, include
+from webdev.home_view import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('task/', include('webdev.task.urls')),
 ]
