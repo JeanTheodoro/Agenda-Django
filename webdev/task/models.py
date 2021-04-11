1b19e5c29f925395be0182c9668601dcffc1670c
JeanTheodoro <jean.theodorosilva@gmail.com>
2021-04-11 16:35:14 -0700

    Created model

--- webdev/task/models.py
+++ webdev/task/models.py
@@ -1,3 +1,6 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
