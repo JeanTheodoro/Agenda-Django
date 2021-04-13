5933ad864dbf694d4cfd7b3d621c4928e54f320d
JeanTheodoro <jean.theodorosilva@gmail.com>
2021-04-12 17:03:11 -0700

    Created teste an feature list taks bagger

--- webdev/task/tests/test_task_get.py
+++ webdev/task/tests/test_task_get.py
@@ -1,10 +1,10 @@
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
-
+from webdev.task.models import Task
 
 @pytest.fixture
-def resposta(client):
+def resposta(client, db):
     res = client.get(reverse('task:home'))
     return res
 
@@ -18,4 +18,21 @@ def test_form_present(resposta):
 def test_button_save_present(resposta):
     assertContains(resposta, '<button type="submit"')
 
-    
+@pytest.fixture
+def list_task_beggar(db):    
+    task = [
+        Task(name='Tak 1', done=False),
+        Task(name='Tak 2', done=False),
+        ]
+    Task.objects.bulk_create(task)
+    return task
+
+
+@pytest.fixture
+def response_list_task(client, list_task_beggar):    
+    resp = client.get(reverse('task:home'))
+    return resp
+
+def response_with_list_task(list_task_beggar,  response_list_task):
+    for task in response_list_task:
+        assertContains(response_list_task, task.name)
--- webdev/task/views.py
+++ webdev/task/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from webdev.task.forms import TaskNewForm
 from django.urls import reverse
+from webdev.task.models import Task
 
 def home(request):
     if request.method == 'POST':
@@ -10,5 +11,7 @@ def home(request):
             form.save()
             return HttpResponseRedirect(reverse('task:home'))
         else:
-          return render(request, 'task/home.html', {'form':form}, status=400)  
-    return render(request, 'task/home.html')
+            task_bagger = Task.objects.filter(done=False).all()
+            return render(request, 'task/home.html', {'form':form, 'task_bagger': task_bagger}, status=400)
+    task_bagger = Task.objects.filter(done=False).all()  
+    return render(request, 'task/home.html', {'task_bagger': task_bagger})
