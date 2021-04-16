d1bfaa02c0982acc688ecedd9d103d0d043251f4
JeanTheodoro <jean.theodorosilva@gmail.com>
2021-04-15 17:19:31 -0700

    Creted list and update task

--- webdev/task/urls.py
+++ webdev/task/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import home 
+from .views import detail
+
 
 app_name = 'task'
 
 urlpatterns = [
-    path('', home, name='home')   
+    path('', home, name='home')  ,
+    path('<int:task_id>', detail, name='detail')
 ]
