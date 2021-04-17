4f052873f81905b751695f778c210449024c1810
JeanTheodoro <jean.theodorosilva@gmail.com>
2021-04-16 18:38:35 -0700

    Created url delete

--- webdev/task/urls.py
+++ webdev/task/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import home 
 from .views import detail
-
+from .views import delete
 
 app_name = 'task'
 
 urlpatterns = [
-    path('', home, name='home')  ,
-    path('<int:task_id>', detail, name='detail')
+    path('', home, name='home'),
+    path('<int:task_id>', detail, name='detail'),
+    path('delete/<int:task_id>', delete, name='delete')
 ]
